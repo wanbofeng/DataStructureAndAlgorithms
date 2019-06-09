555819ba195f8536e511dd382d7d0f1b02ddbff8
wanbofeng <50511740+wanbofeng@users.noreply.github.com>
2019-06-09 15:18:58 +0800

    Add files via upload

---
+++ DataStructures/recursion/queue8.java
@@ -0,0 +1,13 @@
+package com.recursion;
+
+/**
+ * Created by wanbf on 2019/6/9.  八皇后问题
+ */
+public class queue8 {
+
+
+
+
+
+
+}
---
+++ DataStructures/recursion/recursionTest.java
@@ -0,0 +1,11 @@
+package com.recursion;
+
+/**
+ * Created by wanbf on 2019/6/8.
+ */
+public class recursionTest {
+
+
+
+
+}
